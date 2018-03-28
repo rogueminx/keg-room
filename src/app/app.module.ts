b8e1ee071e312102cdd3623faec924f4ff2418eb
Anna Marie Cooper and Kimberly Huynh <kimberly@>
2018-03-28 15:22:15 -0700

    add Firebase info.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,17 @@ import { KegListComponent } from './keg-list/keg-list.component';
 import { EditKegComponent } from './edit-keg/edit-keg.component';
 import { FormsModule } from '@angular/forms';
 import { EmptinessPipe } from './emptiness.pipe';
+import { masterFirebaseConfig } from './api-keys';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 
+
+export const firebaseConfig = {
+  apiKey: masterFirebaseConfig.apiKey,
+  authDomain: masterFirebaseConfig.authDomain,
+  databaseURL: masterFirebaseConfig.databaseURL,
+  storageBucket: masterFirebaseConfig.storageBucket
+};
 
 @NgModule({
   declarations: [
@@ -18,7 +28,9 @@ import { EmptinessPipe } from './emptiness.pipe';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
